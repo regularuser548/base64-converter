38cddf6023a6ce93071d765f8684df2958f7c677
regularuser548 <regularuser548@tutanota.com>
2024-02-18 02:59:40 +0200

    moved js to separated file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<!--suppress HtmlUnknownTarget -->
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -33,7 +34,6 @@
                         </div>
                     </template>
 
-
                     <div class="mb-4">
                         <h2>Convert your images to base64</h2>
 
@@ -59,97 +59,6 @@
             </div>
 
         </div>
-    <script>
-
-        let picturesArray = [];
-
-        function sendAjaxRequests()
-        {
-            let picturesArray = document.getElementById('pictures').files;
-
-            let csrf = document.getElementsByName('_token')[0].value;
-
-            if (picturesArray.length > 10)
-            {
-                setMessage('You can upload up to 10 files at once');
-                return;
-            }
-
-            for (let i = 0; i < picturesArray.length; i++)
-            {
-                const formData = new FormData();
-                formData.append("picture", picturesArray[i]);
-                formData.append('_token', csrf);
-                fetch("{{route('encodeImageAjax')}}",{
-                    method: "POST",
-                    body: formData,
-                    headers: {
-                        "Accept": "application/json",
-                    },
-                }).then(imageEncodingCallback, errorCallback);
-            }
-        }
-
-        function errorCallback()
-        {
-            setMessage("Server is not responding");
-        }
-
-        async function imageEncodingCallback(response)
-        {
-            let res = await response.json();
-            const container = document.getElementById('resultsContainer');
-
-            let template;
-            if (res['errors']) //error
-                template = document.getElementById('errorItemTemplate');
-
-            else //success
-                template = document.getElementById('resultItemTemplate');
-
-            const clone = template.content.cloneNode(true);
-
-            clone.querySelector('.fileName').textContent = res.fileName;
-            clone.querySelector('.fileSize').textContent = res.fileSizeBytes / 1000 + ' KB';
-
-            if (res['errors'])
-            {
-                clone.querySelector('.status').textContent = 'Error';
-                clone.querySelector('.message').textContent = res.errors.picture[0];
-            }
-            else
-            {
-                let length = picturesArray.push(res);
-                clone.querySelector('.status').textContent = 'Success';
-                clone.querySelector('.convertedFileSize').textContent = res.encodedFileSizeBytes / 1000 + ' KB';
-                clone.querySelector('.copyToClipBtn').onclick = async function() { await copyToClipboard(length - 1); };
-            }
-
-            container.appendChild(clone);
-
-            //console.log(picturesArray);
-        }
-
-        async function copyToClipboard(id)
-        {
-            await navigator.clipboard.writeText(picturesArray[id].base64);
-        }
-
-        function setMessage(text)
-        {
-            const container = document.getElementById('messageContainer');
-            let toDelete = container.getElementsByClassName('alert');
-
-            if (toDelete.length > 0)
-                toDelete[0].remove();
-
-            const messageText = text;
-            const message = document.createElement('div');
-            message.className = 'alert alert-danger alert-dismissible fade show';
-            message.innerHTML = `${messageText}<button type="button" class="btn-close" data-bs-dismiss="alert"></button>`;
-            container.appendChild(message);
-        }
-
-    </script>
+    <script src="../../js/app.js"></script>
     </body>
 </html>
